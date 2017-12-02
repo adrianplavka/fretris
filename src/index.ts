3b36f0c484b69954d49a3aa3aa2e3e336f3dcd45
Adrian Plavka <adrian.plavka@gmail.com>
2017-12-02 11:14:16 +0100

    Port bind

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@
 import { server } from './app';
 
 const port = 8000;
-server.listen(port);
+server.listen(process.env.PORT || port);
 console.clear() || console.log("<Tetris Online> @ Index > Started on port", port);
